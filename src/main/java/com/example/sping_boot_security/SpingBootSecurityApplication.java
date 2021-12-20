54e5f6b96b29fa2519d247b3f40e034e9814a52f
connor999jobs <coonor999jobs@gmail.com>
2021-12-20 13:04:58 +0200

    Initial commit

---
+++ src/main/java/com/example/sping_boot_security/SpingBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.sping_boot_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootSecurityApplication.class, args);
+	}
+
+}
